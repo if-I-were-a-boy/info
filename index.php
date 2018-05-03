f3fbb770b61ba31ade8b220590b0fd4354b54998
if-I-were-a-boy <843658276@qq.com>
2018-05-03 15:37:15 +0800

    update

--- index.php
+++ index.php
@@ -3,24 +3,37 @@
  * 过程:设置HTML=>设置采集规则=>执行采集=>获取采集结果数据
  */
 namespace app\index\controller;
+require './spider/phpQuery/phpQuery.php';
+require './spider/QueryList/QueryList.php';
 use QL\QueryList;
-require './vendor/autoload.php';
-require './vendor/jaeger/querylist/src/QueryList.php';
-
-//获取所有的HTML
 header("Content-type:text/html;charset=utf-8");
+//获取所有的HTML
 $url="https://www.nowcoder.com/search?type=post&query=%E5%86%85%E6%8E%A8";
-//$contents=file_get_contents($url);
-//$contents = mb_convert_encoding($contents,'utf-8','gbk');
 
 //编写采集规则
 $rules = array(
- 'title' => array('.discuss-main clearfix','href')
+    'title' => array('.discuss-main clearfix','href')
 );
+
 //列表选择器
 $rang = '.module-body>li';
+
 //开始采集
 $data = QueryList::Query($url,$rules,$rang,'curl');
+
 echo $data->getJSON();
+
+
+//$hj = QueryList::Query('http://mobile.csdn.net/',array("url"=>array('.unit h1 a','href')));
+//$data = $hj->getData(function($x){
+//    return $x['url'];
+//});
+//print_r($data);
+
+
+
+
+//$contents=file_get_contents($url);
+//$contents = mb_convert_encoding($contents,'utf-8','gbk');
 ?>
 
