6077af0ffe1f630e1c3939f70620f4352580d6ca
if-I-were-a-boy <843658276@qq.com>
2018-05-15 18:44:07 +0800

    update

---
+++ mysql/mysql.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: didi
+ * Date: 2018/5/15
+ * Time: 下午5:08
+ */
+class MysqlDb{
+    const info = array(
+        'dbms' => 'mysql',
+        'database' => 'gugu',
+        'table' => 'pushInfo',
+        'user' => 'root',
+        'password' => 'hpuhq',
+        'charset' => 'utf-8',
+        'host' => 'localhost',
+
+    );
+    public function connMysql(){
+        $dbInfo = self::info;
+        $dbms = $dbInfo['dbms'];     //数据库类型
+        $host = $dbInfo['host']; //数据库主机名
+        $dbName = $dbInfo['database'];    //使用的数据库
+        $user = $dbInfo['user'];      //数据库连接用户名
+        $pass= $dbInfo['password'];          //对应的密码
+        $dsn="$dbms:host=$host;dbname=$dbName";
+
+        try {
+            $dbh = new PDO($dsn, $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "set names utf8")); //初始化一个PDO对象
+            return $dbh;
+            /*你还可以进行一次搜索操作
+            foreach ($dbh->query('SELECT * from FOO') as $row) {
+                print_r($row); //你可以用 echo($GLOBAL); 来看到这些值
+            }
+            */
+        } catch (PDOException $e) {
+            die ("Error!: " . $e->getMessage() . "<br/>");
+        }
+    }
+}
---
+++ mysql/pushInfo.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: didi
+ * Date: 2018/5/15
+ * Time: 下午5:08
+ */
+require_once './mysql.php';
+require_once './../tool/opRedis.php';
+require_once './../tool/function.php';
+header("Content-type:text/html;charset=utf-8");
+ini_set('date.timezone','Asia/Shanghai');
+
+$dbn = (new MysqlDb())->connMysql();
+
+
+
+//先导入一批数据进去
+$rules = array(
+    'text' => array('.discuss-detail','html'),
+);
+
+$rang = '.module-body';
+$allInfo = [];
+$flag = false;
+$sumInfo = [];
+for($i=3;$i>=1;$i--) {
+    //爬取符合条件的html
+    $allInfo = [];
+    if($flag){
+        break;
+    }
+    $html = searchInfo($i,$rules, $rang);
+    $info = cutInfo($html);
+    foreach ($info['par_o'][0] as $key => $value) {
+        $perInfo =  getDetail($info['par_o'][0][$key], $info['par_t'][0][$key]);
+            if($perInfo['enable']['date'] < "2018-05-01 ") {
+                $flag = true;
+                break;
+            }
+        if(isset($perInfo["type"]) && $perInfo["type"]){
+            $allInfo[] = $perInfo;
+        }
+    }
+
+    $sumInfo[] = $allInfo;
+
+}
+
+var_dump($sumInfo);
+
+
+
+
+////查询当前查找时间的限制
+//$sql = "select max(time) from Student where true";
+//
+//$res=$dbn->query($sql);
+
+
+
+
+
+?>
