fabed0f0220b24e63558cb754ac4523604c22677
if-I-were-a-boy <843658276@qq.com>
2018-05-16 17:41:08 +0800

    update

--- test.php
+++ test.php
@@ -1,16 +1,26 @@
 <?php
-require_once './mysql.php';
-require_once './../tool/opRedis.php';
-require_once './../tool/function.php';
+require_once './mysql/mysql.php';
+require_once './tool/opRedis.php';
 ini_set('date.timezone','Asia/Shanghai');
 
 $dbn = (new MysqlDb())->connMysql();
-$opRedis = new opRedis();
-
+//$redis = new opRedis();
+$redis = new redis();  
+$redis->connect('127.0.0.1', 6379);  
 $sql = "select * from pushInfo";
 $res = $dbn->query($sql);
-
-foreach ($res as $k => $v){
-
-    $opRedis->addList();
+while($row = $res->fetch()){
+  $id = $row['id'];
+  $s = "pustInfo";
+  $redis->lPush($s,$id);
+//$s = 'pushInfo/'.$id;
+//  $redis -> expire($s,3600*24);
+//  $redis->hset($s,'title',$row['title']); 
+ // $redis->hset($s,'time',$row['time']);
+  //$redis->hset($s,'date',$row['date']);
+ // $redis->hset($s,'detail',$row['detail']);
+ // $redis->hset($s,'enable',$row['enable']);
+ // $redis->hset($s,'expire',$row['expire']);
+ // $redis->hset($s,'title',$row['title']);
 }
+         $redis -> expire("pustInfo",3600*24);
