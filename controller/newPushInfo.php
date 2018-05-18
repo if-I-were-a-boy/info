45d2189913874ad0ff078d3f2a65ba4f8a06dc73
if-I-were-a-boy <843658276@qq.com>
2018-05-18 13:12:34 +0800

    update

--- controller/newPushInfo.php
+++ controller/newPushInfo.php
@@ -2,13 +2,10 @@
 //获取今天最新的内推消息
 require_once "./../index.php";
 require_once "./../mysql/mysql.php";
-
-$dbn = new MysqlDb();
+$dbn = (new MysqlDb())->connMysql();
 $date = date("Y-m-d");
 $sql = 'select * from pushInfo where date = "'.$date.'"';
 $result = $dbn->query($sql);
 $data = $result->fetchAll();
 $len = count($data);
 //return json_encode($date);
-var_dump($len);
-var_dump($result);
