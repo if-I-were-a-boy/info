fa5d781a2d3a3cf88259bd64244c6c4565c2efd4
if-I-were-a-boy <843658276@qq.com>
2018-05-29 11:14:28 +0800

    update

---
+++ controller/books.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: didi
+ * Date: 2018/5/29
+ * Time: 上午10:57
+ */
+$arr[0] = ['book' => 'Redis设计与实现',  'text' => '本书主要分为四大部分。第一部分“数据结构与对象”介绍了Redis中的各种对象及其数据结构，并说明这些数据结构如何影响对象的功能和性能。第二部分“单机数据库的实现”对Redis实现单机数据库的方法进行了介绍，包括数据库、RDB持久化、AOF持久化、事件等。第三部分“多机数据库的实现”对Redis的Sentinel、复制、集群三个多机功能进行了介绍。第四部分“独立功能的实现”对Redis中各个相对独立的功能模块进行了介绍，涉及发布与订阅、事务、Lua脚本、排序、二进制位数组、慢查询日志、监视器等。本书作者专门维护了www.redisbook.com网站，提供带有详细注释的Redis源代码，以及本书相关的更新内容。', 'url' => 'https://pan.baidu.com/s/12LDc-EIdbS-BWLvN6S6zow', 'pass' => 's82n'];
+echo json_encode($arr);
+
