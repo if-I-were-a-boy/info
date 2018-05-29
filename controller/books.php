e6fd6f5c0ae2affceb63cf815651a21a8b548fe7
if-I-were-a-boy <843658276@qq.com>
2018-05-29 12:19:22 +0800

    update

--- controller/books.php
+++ controller/books.php
@@ -5,6 +5,10 @@
  * Date: 2018/5/29
  * Time: 上午10:57
  */
-$arr[0] = ['book' => 'Redis设计与实现',  'text' => '本书全面而完整地讲解了Redis的内部机制与实现方式，对Redis的大多数单机功能以及所有多机功能的实现原理进行了介绍，展示了这些功能的核心数据结构以及关键的算法思想,图示丰富，描述清晰，并给出大量参考信息。通过阅读本书，读者可以快速、有效地了解Redis的内部构造以及运作机制，更好、更高效地使用Redis。', 'url' => 'https://pan.baidu.com/s/12LDc-EIdbS-BWLvN6S6zow', 'pass' => 's82n'];
+$arr[0] = ['book' => 'Redis设计与实现',  'text' => '本书全面而完整地讲解了Redis的内部机制与实现方式，对Redis的大多数单机功能以及所有多机功能的实现原理进行了介绍，展示了这些功能的核心数据结构以及关键的算法思想,图示丰富，描述清晰，并给出大量参考信息。', 'url' => 'https://pan.baidu.com/s/12LDc-EIdbS-BWLvN6S6zow', 'pass' => 's82n'];
+$arr[1] = ['book' => '鸟哥的Linux私房菜基础篇第三版', 'text' => '全面而详细地介绍了Linux操作系统，基本概念的讲解非常细致，深入浅出。各种功能和命令的介绍，都配以大量的实例操作和详尽的解析。本书是初学者学习Linux不可多得的一本入门好书。', 'url' =>'https://pan.baidu.com/s/1SxxVqhjnQp7XUfVt9-pVsw', 'pass' =>'6tnp'];
+$arr[2] = ['book' => 'JavaScript权威指南_第6版', 'text' => '本书是程序员学习核心JavaScript语言和由Web浏览器定义的JavaScript API的指南和综合参考手册。本书适合那些希望学习Web编程语言的初、中级程序员和希望精通JavaScript的JavaScript程序员阅读。', 'url' => 'https://pan.baidu.com/s/1-PAUMVj-Tf820uh9dkYjNA', 'pass'=>'aycc'];
+$arr[3] = ['book' => '深入理解Nginx', 'text'=> '本书重点介绍Nginx的基本用法和配置规则，如何开发HTTP模块来得到定制的Nginx，综合Nginx框架代码分析Nginx的架构，介绍其设计理念和技巧，进一步帮助读者自由、有效地开发出功能丰富、性能一流的Nginx模块', 'url' =>'https://pan.baidu.com/s/1iVPJBFW8ec-Wp7UsbP0H8Q', 'pass' =>'j4nu'];
+$arr[4] = ['book' => 'UNIX网络编程第二卷', 'text'=> '本书全面深入地介绍了4种IPC形式：消息传递(管道、FIFO、消息队列)、同步(互斥锁、条件变量、读写锁、文件与记录锁、信号量)、共享内存(匿名共享内存、具名共享内存)及远程过程调用(Solaris 门、Sun RPC)。', 'url' => 'https://pan.baidu.com/s/1ulyaDBWH6VNU8fcSDWHiXw', 'pass' =>'7rz2'];
 echo json_encode($arr);
 
